b470db9a57c7e09ab37a4789443cbbe21d7549ea
igrave <isaac.gravestock@gmail.com>
2025-02-05 22:43:09 +0100

    get authtoken

--- run.R
+++ run.R
@@ -10,7 +10,7 @@ service_account <- 'ladder-testing-account@helpful-range-376621.iam.gserviceacco
 access_token_lifetime <- '300s' # optional, default: '3600s' (1 hour)
 access_token_scopes <-  'https://www.googleapis.com/auth/drive.file'
 create_credentials_file <- TRUE
-
+universe <- "googleapis.com"
 
 oidcTokenAudience <- paste0("https://iam.googleapis.com/", workload_identity_provider)
 
@@ -20,7 +20,9 @@ oidcToken <- httr::GET(
     add_headers(Authorization = paste0("Bearer ", id_token_request_token)),
     query = list(audience = oidcTokenAudience)
 )
-content(oidcToken)
+content(oidcToken)$value
+
+# get authtoken -----------------
 
 # client = new WorkloadIdentityFederationClient({
 #         logger: logger,
@@ -34,3 +36,58 @@ content(oidcToken)
 #         workloadIdentityProviderName: workloadIdentityProvider,
 #         serviceAccount: serviceAccount,
 #       })
+#
+##[debug]WorkloadIdentityFederationClient.getToken: Built request, {
+##[debug]  "method": "POST",
+##[debug]  "path": "https://sts.googleapis.com/v1/token",
+##[debug]  "headers": {},
+##[debug]  "body": {
+##[debug]    "audience": "//iam.googleapis.com/projects/1073903696751/locations/global/workloadIdentityPools/github/providers/my-repo",
+##[debug]    "grantType": "urn:ietf:params:oauth:grant-type:token-exchange",
+##[debug]    "requestedTokenType": "urn:ietf:params:oauth:token-type:access_token",
+##[debug]    "scope": "https://www.googleapis.com/auth/cloud-platform",
+##[debug]    "subjectTokenType": "urn:ietf:params:oauth:token-type:jwt",
+##[debug]    "subjectToken": "***"
+##[debug]  }
+##[debug]}
+
+make_endpoints <- function(universe) {
+    endpoints <- list(
+     iam = "https://iam.{universe}/v1",
+     iamcredentials = "https://iamcredentials.{universe}/v1",
+     oauth2 = "https://oauth2.{universe}",
+     sts = "https://sts.{universe}/v1",
+     www = "https://www.{universe}"
+    )
+    sub("{universe}", universe, endpoints, fixed = TRUE)
+}
+#   protected readonly _endpoints = {
+#     iam: 'https://iam.{universe}/v1',
+#     iamcredentials: 'https://iamcredentials.{universe}/v1',
+#     oauth2: 'https://oauth2.{universe}',
+#     sts: 'https://sts.{universe}/v1',
+#     www: 'https://www.{universe}',
+#   };
+endpoints <- make_endpoints(universe)
+
+    #   audience: this.#audience,
+    #   grantType: `urn:ietf:params:oauth:grant-type:token-exchange`,
+    #   requestedTokenType: `urn:ietf:params:oauth:token-type:access_token`,
+    #   scope: `${this._endpoints.www}/auth/cloud-platform`,
+    #   subjectTokenType: `urn:ietf:params:oauth:token-type:jwt`,
+    #   subjectToken: this.#githubOIDCToken,
+
+authtoken <- httr::POST(
+    url = paste0(endpoints[["sts"]], "/token"),
+    body = list(
+        audience = paste0("//", parse_url(oidcTokenAudience)$hostname, "/", workload_identity_provider),
+        grantType = "urn:ietf:params:oauth:grant-type:token-exchange",
+        requestedTokenType = "urn:ietf:params:oauth:token-type:access_token",
+        scope = paste0(endpoints[["www"]], "/auth/cloud-platform"),
+        subjectTokenType = "urn:ietf:params:oauth:token-type:jwt",
+        subjectToken = content(oidcToken)$value
+
+    )
+)
+authtoken
+contents(authtoken)
