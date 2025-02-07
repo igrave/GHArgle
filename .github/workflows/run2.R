012a4efb622912404b33057cd0256ff68b583656
igrave <isaac.gravestock@gmail.com>
2025-02-07 23:11:12 +0100

    try this!

---
+++ .github/workflows/run2.R
@@ -0,0 +1,9 @@
+remotes::install_github("igrave/gargle")
+
+credentials_github_actions(
+    project_id = 'helpful-range-376621',
+  workload_identity_provider = 'projects/1073903696751/locations/global/workloadIdentityPools/github/providers/my-repo',
+  service_account = 'ladder-testing-account@helpful-range-376621.iam.gserviceaccount.com',
+  lifetime = '3600s',
+  scopes = "https://www.googleapis.com/auth/drive.file",
+)
