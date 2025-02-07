9ed63bc54d78caf23d9d5d8a7cd18d718cadad3d
igrave <isaac.gravestock@gmail.com>
2025-02-07 23:17:56 +0100

    library

--- .github/workflows/run2.R
+++ .github/workflows/run2.R
@@ -1,5 +1,5 @@
 remotes::install_github("igrave/gargle")
-
+library(gargle)
 credentials_github_actions(
     project_id = 'helpful-range-376621',
   workload_identity_provider = 'projects/1073903696751/locations/global/workloadIdentityPools/github/providers/my-repo',
