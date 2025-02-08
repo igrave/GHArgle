8ce866b449b8ba3b66d23b3db4c196e9f913b060
igrave <isaac.gravestock@gmail.com>
2025-02-08 08:50:49 +0100

    try

--- run.R
+++ run.R
@@ -1,5 +1,9 @@
 library(gargle)
 options(gargle_verbosity = "debug")
+
+
+
+try(
 credentials_github_actions(
     project_id = 'helpful-range-376621', 
     workload_identity_provider = 'projects/1073903696751/locations/global/workloadIdentityPools/github/providers/my-repo',
@@ -7,11 +11,22 @@ credentials_github_actions(
     lifetime = '3600s',
     scopes = 'https://www.googleapis.com/auth/drive.file'
     )
+}
+
+try(
+with_gargle_verbosity(
+    "info",
+    credentials_github_actions(
+    project_id = 'helpful-range-376621', 
+    workload_identity_provider = 'projects/1073903696751/locations/global/workloadIdentityPools/github/providers/my-repo',
+    service_account = 'ladder-testing-account@helpful-range-376621.iam.gserviceaccount.com',
+    lifetime = '3600s',
+    scopes = 'https://www.googleapis.com/auth/drive.file'
+    )
+)
 
 
-
-
-
+)
 library(httr)
 
 id_token_url <- Sys.getenv("ACTIONS_ID_TOKEN_REQUEST_URL")
