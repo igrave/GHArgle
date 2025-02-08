c49957a26454d83d4ca3000eb45f9dc49f302325
igrave <isaac.gravestock@gmail.com>
2025-02-08 08:21:51 +0100

    verbose

--- .github/workflows/run2.R
+++ .github/workflows/run2.R
@@ -1,9 +1,3 @@
-remotes::install_github("igrave/gargle")
+remotes::install_github("igrave/gargle@GHArgle")
 library(gargle)
-credentials_github_actions(
-    project_id = 'helpful-range-376621',
-  workload_identity_provider = 'projects/1073903696751/locations/global/workloadIdentityPools/github/providers/my-repo',
-  service_account = 'ladder-testing-account@helpful-range-376621.iam.gserviceaccount.com',
-  lifetime = '3600s',
-  scopes = "https://www.googleapis.com/auth/drive.file",
-)
+gargle::credentials_github_actions(project_id = 'helpful-range-376621', workload_identity_provider = 'projects/1073903696751/locations/global/workloadIdentityPools/github/providers/my-repo', service_account = 'ladder-testing-account@helpful-range-376621.iam.gserviceaccount.com', lifetime = '3600s', scopes = 'https://www.googleapis.com/auth/drive.file')
--- run.R
+++ run.R
@@ -1,3 +1,17 @@
+library(gargle)
+options(gargle_verbosity = "debug")
+credentials_github_actions(
+    project_id = 'helpful-range-376621', 
+    workload_identity_provider = 'projects/1073903696751/locations/global/workloadIdentityPools/github/providers/my-repo',
+    service_account = 'ladder-testing-account@helpful-range-376621.iam.gserviceaccount.com',
+    lifetime = '3600s',
+    scopes = 'https://www.googleapis.com/auth/drive.file'
+    )
+
+
+
+
+
 library(httr)
 
 id_token_url <- Sys.getenv("ACTIONS_ID_TOKEN_REQUEST_URL")
