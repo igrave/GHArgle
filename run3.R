0f0094c1323b4cb21a8a5441c8d467ca43cdd651
igrave <isaac.gravestock@gmail.com>
2025-02-09 22:50:35 +0100

    clean

--- run3.R
+++ run3.R
@@ -3,18 +3,6 @@ library(ladder)
 options(gargle_verbosity = "debug")
 
 
-ladder_auth
-
-try(
-credentials_github_actions(
-    project_id = 'helpful-range-376621', 
-    workload_identity_provider = 'projects/1073903696751/locations/global/workloadIdentityPools/github/providers/my-repo',
-    service_account = 'ladder-testing-account@helpful-range-376621.iam.gserviceaccount.com',
-    lifetime = '3600s',
-    scopes = 'https://www.googleapis.com/auth/drive.file'
-    )
-)
-
 ladder::ladder_auth_gha_workflow(
     project_id = 'helpful-range-376621', 
     workload_identity_provider = 'projects/1073903696751/locations/global/workloadIdentityPools/github/providers/my-repo',
